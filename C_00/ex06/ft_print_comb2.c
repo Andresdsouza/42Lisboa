354ecbe82c82967797943d03fabe39aaa6f84750
Andresdsouza <114589808+Andresdsouza@users.noreply.github.com>
2022-09-28 16:27:50 +0100

    Add files via upload

---
+++ C_00/ex06/ft_print_comb2.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asouza <asouza@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/13 12:50:24 by asouza            #+#    #+#             */
+/*   Updated: 2022/09/13 14:28:12 by asouza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char a)
+{
+	write(1, &a, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = -1;
+	while (a++ < 98)
+	{
+		b = a;
+		while (b++ < 99)
+		{
+			ft_putchar((a / 10) + '0');
+			ft_putchar((a % 10) + '0');
+			ft_putchar(' ');
+			ft_putchar((b / 10) + '0');
+			ft_putchar((b % 10) + '0');
+			if (a != 98)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+		}
+	}
+}
+
+/*int main()
+{
+	ft_print_comb2();
+}*/
